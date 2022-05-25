43967c0c5431e7ee20c2b3dee3d9acb6f22806eb
Benjamin McCown <mccownbm@amazon.com>
2022-05-24 19:13:05 -0500

    feat: add my custom qwertyoku config

--- keyboards/crkbd/keymaps/miryoku-3x6/config.h
+++ keyboards/crkbd/keymaps/miryoku-3x6/config.h
@@ -7,7 +7,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -38,6 +38,19 @@
   #define COMBO_TERM 200
   #define EXTRA_SHORT_COMBOS
 #endif
+
+#define LAYOUT_miryoku(\
+       K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
+       K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
+       K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
+       N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
+)\
+LAYOUT_split_3x6_3(\
+KC_TAB, K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,   KC_BSPC,\
+KC_ESC, K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,   KC_QUOT,\
+KC_LSFT, K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,   KC_RSFT,\
+                     K32,   K33,   K34,          K35,   K36,   K37\
+)
 
 #include "custom_config.h"
 
